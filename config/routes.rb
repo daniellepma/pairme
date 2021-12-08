59e5ee4d6db979fd528471f4f7af30a05c592826
anonymous anonymous <danielle.ma@kellogg.northwestern.edu>
2021-12-08 17:21:46 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "cuisines#index"
   resources :saved_pairings
   resources :dish_pairings
