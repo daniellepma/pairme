d2d134b7b3328dacb046ca8e085a6304821f869e
anonymous anonymous <danielle.ma@kellogg.northwestern.edu>
2021-12-08 17:21:59 +0000

    Whitelist columns in SavedPairing for ActiveAdmin

--- app/admin/saved_pairings.rb
+++ app/admin/saved_pairings.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register SavedPairing do
 
+ permit_params :user_id, :pairing_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
