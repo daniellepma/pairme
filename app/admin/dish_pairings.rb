c34dad651f3623fe46ebfde88a88398c791b3f6b
anonymous anonymous <danielle.ma@kellogg.northwestern.edu>
2021-12-08 17:21:57 +0000

    Whitelist columns in DishPairing for ActiveAdmin

--- app/admin/dish_pairings.rb
+++ app/admin/dish_pairings.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register DishPairing do
 
+ permit_params :dish_id, :wine_id, :description, :share_link
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
